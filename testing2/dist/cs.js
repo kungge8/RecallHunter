7956bdd084f1b7ff160cfadb67828ce43a1c4c40
kungge8 <kung.ge8@gmail.com>
2017-11-03 11:20:20 -0400

    adding gitignore

---
+++ testing2/dist/cs.js
@@ -0,0 +1,103 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 121);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 121:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25';
+var vendor = 'Apple, Inc.';
+var platform = 'iPhone';
+
+var a = document.createElement('script');
+
+a.type = 'text/javascript';
+
+a.innerText += '\n Object.defineProperty(window.navigator, \'userAgent\', { get: () => { return \'' + ua + '\'; } });\n Object.defineProperty(window.navigator, \'vendor\', { get: () => { return \'' + vendor + '\'; } });\n Object.defineProperty(window.navigator, \'platform\', { get: () => { return \'' + platform + '\'; } });\n';
+
+document.documentElement.insertBefore(a, document.documentElement.firstChild);
+
+document.addEventListener('beforeload', function (e) {
+  Object.defineProperty(window.navigator, 'userAgent', { get: function get() {
+      return ua;
+    } });
+  Object.defineProperty(window.navigator, 'vendor', { get: function get() {
+      return vendor;
+    } });
+  Object.defineProperty(window.navigator, 'platform', { get: function get() {
+      return platform;
+    } });
+}, true);
+
+/***/ })
+
+/******/ });
