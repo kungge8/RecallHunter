3df924b64379e77c297b8e24074cbd966a700ee7
kungge8 <kung.ge8@gmail.com>
2017-11-08 12:45:51 -0500

    testing new user, altered user model

--- server/models/User.js
+++ server/models/User.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
+  // email: {
+  //   type: String,
+  //   required: true
+  // },
+  // password: {
+  //   type: String,
+  //   required: true
+  // },
   watchlist: [{
     type: Schema.Types.ObjectId,
     ref: "Product"
